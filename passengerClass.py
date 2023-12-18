8928c6b0f9cb19f4b826baf4eefd8258f6352af5
Atodiresei Andrei-Mihai <atodireseiandrei833@yahoo.com>
2023-12-18 16:45:02 +0200

    docstringed passengerClass

--- passengerClass.py
+++ passengerClass.py
@@ -10,22 +10,52 @@ class Passenger():
     def __repr__(self) -> str:
         return (f"{self.__first_name} {self.__last_name}")
 
-    #getters
+    # Getters
     def get_first_name(self):
+        """
+        D: Retrieves the first name of the passenger.
+        I: None
+        O: Returns the first name of the passenger.
+        """
         return self.__first_name
-    
+
     def get_last_name(self):
+        """
+        D: Retrieves the last name of the passenger.
+        I: None
+        O: Returns the last name of the passenger.
+        """
         return self.__last_name
-    
+
     def get_passport_nr(self):
+        """
+        D: Retrieves the passport number of the passenger.
+        I: None
+        O: Returns the passport number of the passenger.
+        """
         return self.__passport_nr
-    
-    #setters
-    def set_first_name(self,new_first_name):
+
+    # Setters
+    def set_first_name(self, new_first_name):
+        """
+        D: Sets a new first name for the passenger.
+        I: new_first_name (str) - The new first name to set.
+        O: None
+        """
         self.__first_name = new_first_name
 
-    def set_last_name(self,new_last_name):
+    def set_last_name(self, new_last_name):
+        """
+        D: Sets a new last name for the passenger.
+        I: new_last_name (str) - The new last name to set.
+        O: None
+        """
         self.__last_name = new_last_name
 
-    def set_passport_nr(self,new_passort_nr):
-        self.__passport_nr = new_passort_nr
+    def set_passport_nr(self, new_passport_nr):
+        """
+        D: Sets a new passport number for the passenger.
+        I: new_passport_nr (str) - The new passport number to set.
+        O: None
+        """
+        self.__passport_nr = new_passport_nr
